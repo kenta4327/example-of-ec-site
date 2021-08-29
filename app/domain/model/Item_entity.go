330350f2a8d2d6c106e91af1e8f7e9e22a54b30b
kenta4327 <kentanke_15_m@hotmail.com>
2021-08-29 20:44:25 +0900

    hoge

--- app/domain/model/Item_entity.go
+++ app/domain/model/Item_entity.go
@@ -8,14 +8,14 @@ type ItemEntity struct {
 
 func NewItemEntity(id int, name string, price int) *ItemEntity {
 	return &ItemEntity{
-id:id,
-name:  name,
-price: price,
+		id:id,
+		name:  name,
+		price: price,
 	}
 }
 
 func (itemEntity ItemEntity) Id() int {
-	return itemEntity.id
+return itemEntity.id
 }
 
 func (itemEntity ItemEntity) Name() string {
